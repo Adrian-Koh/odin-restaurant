f3754d6caf59f0744019c7023df545fbfc6de894
Adrian-Koh <adriankoh216@yahoo.com>
2025-06-17 16:48:29 +0800

    Implement buttons functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import {createHome, createMenuItems, createAboutItems} from './content.js';
 import './styles.css';
 
+const homeBtn = document.querySelector('#home');
+const menuBtn = document.querySelector('#menu');
+const aboutBtn = document.querySelector('#about');
 
-createAboutItems();
+function clearContent() {
+    const content = document.querySelector('#content');
+    content.innerHTML = '';
+}
+
+homeBtn.addEventListener('click', () => {
+    clearContent();
+    createHome();
+});
+
+menuBtn.addEventListener('click', () => {
+    clearContent();
+    createMenuItems();
+});
+
+aboutBtn.addEventListener('click', () => {
+    clearContent();
+    createAboutItems();
+});
+
+
+createHome();
