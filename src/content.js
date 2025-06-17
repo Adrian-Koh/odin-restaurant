7d7f20318d4999a86d4e3c672eb4cf33db744069
Adrian-Koh <adriankoh216@yahoo.com>
2025-06-17 18:21:19 +0800

    Add the rest of menu items

--- src/content.js
+++ src/content.js
@@ -19,9 +19,9 @@ function createHome() {
     const info = document.createElement('p');
     info.id = 'restaurant-info';
     info.innerText = `Welcome to Odin Restaurant! Here you will find various dishes from all backgrounds. 
-                      We serve Italian, Chinese, Korean, Japanese, and Brazilian cuisine.
+                      We serve Italian, Mexican, Chinese, Korean and Japanese cuisines.
                       Our ingredients are sourced from the freshest and most premium of sources while
-                      our chefs come from the prestigious culinary academies and boast years of experience 
+                      our chefs come from prestigious culinary academies and boast years of experience 
                       in some of the world's best restaurants. We hope you enjoy your time here!`;
     homeContainer.appendChild(info);
     content.appendChild(homeContainer);
@@ -108,22 +108,30 @@ function createMenuItems() {
     const friedRice = new MenuItem('Fried Rice', 'Aromatic short-grain rice stir fried with egg, chicken and bell peppers.');
     const bibimbap = new MenuItem('Bibimbap', 'Rice mixed with gochujang, fried egg, minced beef and vegetables in a stone pot.');
     const unagidon = new MenuItem('Unagi Don', 'Smoked eel laid on a bed of rice, accompanied with a serving of eel sauce.');
+    const ceviche = new MenuItem('Ceviche', 'Fresh fish caught today from Vancouver, B.C. marinated in lime juice and spices.');
+    const pizza = new MenuItem('Pizza', 'Traditional wood-fired margherita pizza.');
 
     const foodCategory = new MenuCategory('Food');
     foodCategory.addItem(pasta);
     foodCategory.addItem(friedRice);
     foodCategory.addItem(bibimbap);
     foodCategory.addItem(unagidon);
+    foodCategory.addItem(ceviche);
+    foodCategory.addItem(pizza);
 
 
     const cocacola = new MenuItem('Coca cola', 'A refreshing chilled can of coca cola, served in a glass with lemon.');
     const ribena = new MenuItem('Ribena', 'Everyone\'s favorite childhood drink, blackcurrant flavored.')
     const orangeJuice = new MenuItem('Orange juice', 'Freshly squeezed juice from navel oranges imported from Australia.')
+    const whiteWine = new MenuItem('White Wine', 'White wined aged over 3 decades from southern France.');
+    const soju = new MenuItem('Soju', 'Plum flavored soju from South Korea.');
 
     const bevCategory = new MenuCategory('Beverages');
     bevCategory.addItem(cocacola);
     bevCategory.addItem(ribena);
     bevCategory.addItem(orangeJuice);
+    bevCategory.addItem(whiteWine);
+    bevCategory.addItem(soju);
 
     const menu = new Menu();
     menu.addCategory(foodCategory);
@@ -173,10 +181,10 @@ class About {
 
 function createAboutItems() {
     const location = new AboutItem('Location', '2800 Wayzata Dr., Minneapolis MN 55425.');
-    const hours = new AboutItem('Hours', 'Mon-Fri: 11am-11pm\nSat: 12pm-1am\nSun: 12pm-2am');
+    const hours = new AboutItem('Hours', 'Mon-Fri: 11 a.m.-11 p.m.\nSat: 12 p.m.-1 a.m.\nSun: 12 p.m.-2 a.m.');
     const history = new AboutItem('Origin', 
         `Odin Restaurant started as a mom-and-pop shop in the 70\'s in rural Minnesota, serving patrons delicious food after a long day of work. 
-         As our vision grew, we wanted to serve the best versions of our dishes and it has grown into the go-to spot for premium dishes in the Twin Cities.`);
+         As our vision grew, we wanted to serve the best versions of our dishes and it has grown into the go-to spot for premium dishes in the Twin Cities that it is today.`);
 
     const about = new About();
     about.addItem(location)     ;
